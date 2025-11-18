a3ccdfbcfe18002128ae30af3a1e15cc6d828eea
Leow14 <leonardoamorimdearaujo@hotmail.com>
2025-11-18 00:16:56 -0300

    Adição de resoluções do exercício 1342 do leet code de duas ḿaneiras diferentes.

---
+++ Binários.py/1234.NumberOfSteps binaryVersion.py
@@ -0,0 +1,17 @@
+# Fazendo agora a mesma solução porém com operadores binários.
+
+class Solution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        steps = 0
+        while num:
+            if num & 1 == 1: # é ímpar
+                num -= 1
+            else:
+                num >>= 1
+            steps += 1
+
+        return steps
---
+++ Binários.py/1234.NumberOfStepsToReduceNumberToZero.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        steps = 0
+        while num > 0:
+            if num%2:
+                num -= 1
+            else:
+                num /= 2
+            steps += 1
+    
+        return steps
