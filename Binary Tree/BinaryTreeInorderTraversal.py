28ad406d6c1dd7db44815a19f78d3900d6e56406
Leow14 <leonardoamorimdearaujo@hotmail.com>
2026-01-02 12:41:44 -0300

    resolução do exercício 94 da plataforma leetcode. https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/1872197159/

---
+++ Binary Tree/BinaryTreeInorderTraversal.py
@@ -0,0 +1,16 @@
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        result = []
+        self._inorder(root, result)
+        return result
+
+    def _inorder(self, node, result):
+        if node:
+            self._inorder(node.left, result)
+            result.append(node.val)
+            self._inorder(node.right, result)
